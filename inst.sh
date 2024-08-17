8056599ae30b0de6ad4875bf1ead9de14f891d76
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-17 18:10:54 +0700

    inst.sh

---
+++ inst.sh
@@ -0,0 +1,12 @@
+clear
+echo "Đang cài đặt"
+apt update && apt upgrade -y
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/v111.zip && unzip 4gtun.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/t1 && mv t1 motd
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/l1 && chmod +x * && mv l1 login &&
+clear
+echo "Đã cài đặt thành công"
+login
