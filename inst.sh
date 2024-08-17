1d5350afe0ccf5227d2a4b54db61956871686cad
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-17 18:57:29 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -2,11 +2,11 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/4gtun.zip && unzip 4gtun.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/4gtun.zip && unzip 4gtun.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/t1 && mv t1 motd
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gtun/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
