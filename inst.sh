8f4bd7ae242a5591c5eea912a01e794cf6f958de
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-22 20:23:42 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -5,7 +5,7 @@ sleep 1
 echo -e "\e[1;33mĐang cài đặt\e[0m"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x nek && chmod a+x nekman.go && chmod a+x 4g && chmod a+x psiphon-tunnel-core && chmod a+x qex && chmod a+x wl
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x nek && chmod a+x nekman.go && chmod a+x 4g && chmod a+x qex && chmod a+x psiphon-tunnel-core  
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/t1 && mv t1 motd
 clear
@@ -16,5 +16,4 @@ echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
 sleep 1
 clear
 termux-wake-lock
-clear
 login
