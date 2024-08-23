49f66f692c07f00df85fb816a9ff447c01dd2059
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-23 09:32:45 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -1,10 +1,14 @@
 clear
+echo -e "\e[1;33mCấp quyền truy cập bộ nhớ thiết bị\e[0m"
+sleep 1
 termux-setup-storage
 clear
-sleep 1
-echo -e "\e[1;33mĐang cài đặt\e[0m"
+sleep 3
+echo -e "\e[1;33m Cập nhật pakages\e[0m"
 pkg update -y
 clear
+echo -e "\e[1;33m Đang cài đặt\e[0m"
+sleep1
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x *  
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/t1 && mv t1 motd
@@ -15,5 +19,8 @@ sleep 1
 echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
 sleep 1
 clear
+echo -e "\e[1;33mBật chạy nền\e[0m"
+sleep 1
 termux-wake-lock
+sleep 2
 login
